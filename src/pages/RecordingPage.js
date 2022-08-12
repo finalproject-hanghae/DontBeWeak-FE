de439408daf0fec34cb5cd65cb210bf072869a95
Tak2 <tinny0505@gmail.com>
2022-08-12 21:12:51 +0900

    Create CalenderSection

--- src/pages/RecordingPage.js
+++ src/pages/RecordingPage.js
@@ -6,13 +6,15 @@ import NotFoundModal from "../components/modals/NotFoundModal";
 import SideFriendBar from "../components/SideFriendBar";
 import { ColumnFlexDiv, PageSection, RowFlexDiv } from "../style/styled";
 
+import CalenderSection from "../components/CalenderSection"
+
 const RecordingPage = () => {
   const navigate = useNavigate();
   return (
     <PageSection>
       <ColumnFlexDiv className="Padding">
-        {/* 달력 컴포넌트 만들어서 집어넣기 */}
-        <div>달력</div>
+        <CalenderSection/>
+        
         {/* 체크박스박스 컴포넌트 만들어서 집어넣기 */}
         <div>체크박스박스</div>
       </ColumnFlexDiv>
