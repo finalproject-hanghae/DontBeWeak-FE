debfa139ea1b18923d83f83c930d5986cd3552e4
인소현 <sohyun7305@naver.com>
2022-08-16 16:03:11 +0900

    Issues#50 Refactor

--- src/components/layout/modal/modalforms/SignUpForm.js
+++ src/components/layout/modal/modalforms/SignUpForm.js
@@ -1,26 +1,48 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { ColumnFlexDiv } from "../../../../style/styled";
+import axios from "axios";
 
 function SignUpForm() {
+  const navigate = useNavigate();
+
   const signUpIdRef = React.useRef(); // Id
-  const signUpNameRef = React.useRef(); // Name
+  const signUpNicknameRef = React.useRef(); // nickName
   const signUpPwRef = React.useRef(); // Pw
   const signUpPwCheckRef = React.useRef(); // PwCheck
   const signUpAlertRef = React.useRef(); // 경고
 
-  const submitToSignUp = (e) => {
+  const submitToSignUp = async (e) => {
     e.preventDefault();
     const username = signUpIdRef.current.value;
-    const name = signUpNameRef.current.value;
+    const nickname = signUpNicknameRef.current.value;
     const password = signUpPwRef.current.value;
     const checkPassword = signUpPwCheckRef.current.value;
+
+    // axios 요청 보낼 자리
+    try {
+      await axios({
+        method: "post",
+        url: "http://3.37.88.75/user/signup",
+        data: {
+          username: username,
+          nickname: nickname,
+          password: password,
+          passwordCheck: checkPassword,
+        },
+      }).then((response) => console.log(response));
+      navigate("/login")
+    } catch (error) {
+      alert(error.response.data.message);
+      console.log(error);
+    }
 
     // 공백 유효성 검사
     if (username === "") {
       signUpAlertRef.current.innerText = "아이디를 입력하세요.";
       return;
-    } else if (name === "") {
-      signUpAlertRef.current.innerText = "이름을 입력하세요.";
+    } else if (nickname === "") {
+      signUpAlertRef.current.innerText = "닉네임을 입력하세요.";
       return;
     } else if (password === "") {
       signUpAlertRef.current.innerText = "비밀번호를 입력하세요.";
@@ -29,10 +51,6 @@ function SignUpForm() {
       signUpAlertRef.current.innerText = "비밀번호를 다시 입력하세요.";
       return;
     }
-
-    console.log(username, name, password, checkPassword);
-
-    // axios 요청 보낼 자리
   };
 
   return (
@@ -40,7 +58,7 @@ function SignUpForm() {
       <ColumnFlexDiv>
         <h2>회원가입</h2>
         <input type="text" placeholder="ID" ref={signUpIdRef} />
-        <input type="text" placeholder="User name" ref={signUpNameRef} />
+        <input type="text" placeholder="User name" ref={signUpNicknameRef} />
         <input type="password" placeholder="PW" ref={signUpPwRef} />
         <input type="password" placeholder="PW check" ref={signUpPwCheckRef} />
         <small ref={signUpAlertRef} />
