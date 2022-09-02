c30aad4deea254eccc0ec5b3ba684fe6502cf0db
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2022-09-02 11:23:00 +0900

    JS2TSX file type

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import store from "./redux/configStore";
 
 import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <Provider store={store}>
     <BrowserRouter>
