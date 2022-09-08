1f8ad10cfc3b4559ec11379a0e24d2e93ca372d8
인소현 <sohyun7305@naver.com>
2022-09-08 23:04:18 +0900

    Conplict..Api..

--- src/api/catApi.js
+++ src/api/catApi.js
@@ -3,19 +3,18 @@ import axios from "axios";
 import applyInterceptorsTo from "./axiosInterceptors";
 
 const baseApi = axios.create({
-  baseURL: process.env.REACT_APP_DB_HOST + "cat",
+  baseURL: process.env.REACT_APP_DB_HOST,
 });
 
 const useThisApi = applyInterceptorsTo(baseApi);
 
-//cat 관련 API -> catApi
 const apiMyCatStatus = async () => {
-  const dataApiCatStatus = await useThisApi.get("");
+  const dataApiCatStatus = await useThisApi.get("/cat");
   return dataApiCatStatus;
 };
 
 const apiSomeCatStatus = async (path) => {
-  const dataApiSomeCatStatus = await useThisApi.get(`/${path}`);
+  const dataApiSomeCatStatus = await useThisApi.get(`/cat/${path}`);
   return dataApiSomeCatStatus;
 };
 
