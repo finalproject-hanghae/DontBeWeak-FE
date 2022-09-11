3b28d9b82795a2bfd949092845d3f466f30a8f41
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2022-09-11 14:39:32 +0900

    Fixed Critical Issue w/ Calender

--- src/components/purpose/calender/DateViewCard.tsx
+++ src/components/purpose/calender/DateViewCard.tsx
@@ -16,7 +16,7 @@ const DateViewCard = ({ day, myWeek, array }: GreetingsProps) => {
       <h3>{day}</h3>
       {myWeek &&
         myWeek
-          .filter((value, index) => value.dayOfWeekValue == array.indexOf(day))
+          .filter((value, index) => new Date(value.usedAt).getDay() == array.indexOf(day))
           .map((val, index) => {
             return (
               <div
